95d3ab41e0997013e22979899c0794a386abe89b
Anish <anish.munirathinam@gmail.com>
2023-05-31 15:46:13 +0530

    feat: Inital commit

---
+++ ReadMe/ReadMe/App/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Munirathinam, Anish | RIEPL on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RMBookListView()
+        }
+    }
+}
